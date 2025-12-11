c3055432ec3c453fbd91a6b7c0621cdeadfc2cf5
2100030301 <2100030301@kluniversity.n>
2025-12-11 12:05:23 +0530

    Comprehensive responsive design fixes: overflow prevention, mobile optimizations, layout improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,16 @@ import { CircuitBackground } from "./components/CircuitBackground";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen overflow-hidden">
-      <CircuitBackground>
-        <div className="w-full">
-          <Navbar />
-          <HeroSection />
-        </div>
-      </CircuitBackground>
-      <Footer />
+    <main className="flex flex-col min-h-screen overflow-x-hidden w-full max-w-full">
+      <Navbar />
+      <div className="pt-[56px] sm:pt-[64px] lg:pt-[70px] w-full">
+        <CircuitBackground>
+          <div className="w-full">
+            <HeroSection />
+          </div>
+        </CircuitBackground>
+        <Footer />
+      </div>
     </main>
   );
 }
